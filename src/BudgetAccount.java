a990fc89302104c5709c85006167802a1d4e8b4b
Peter M Franklin <petermfranklin@Peters-Mac-Studio.local>
2026-05-27 22:17:32 -0700

    Initial commit - Transaction class

---
+++ src/BudgetAccount.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BudgetAccount {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your budget transactions: ");
+        
+    }
+}
